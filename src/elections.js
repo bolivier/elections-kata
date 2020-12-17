8190c38e34181bcece45b7d251d6c4b111469e09
Brandon Olivier <bolivier@foreflight.com>
2020-12-17 13:51:37 -0600

    Hopefully remove Java stuff

--- src/elections.js
+++ src/elections.js
@@ -47,5 +47,105 @@ class Elections {
 		}
 	}
 
-	results() {}
+	results() {
+		const results = {};
+		const nbVotes = 0;
+		const nullVotes = 0;
+		const blankVotes = 0;
+		const nbValidVotes = 0;
+
+		if (!withDistrict) {
+			nbVotes = this.votesWithoutDistricts.reduce(
+				(acc, elm) => acc + elm
+			);
+			for (let i = 0; i < this.officialCandidates.length; i++) {
+				const index = candidates.indexOf(officialCandidates[i]);
+				nbValidVotes += votesWithoutDistricts[index];
+			}
+
+			for (let i = 0; i < this.votesWithoutDistricts.length; i++) {
+				const candidatResult =
+					(this.votesWithoutDistricts[i] * 100) / nbValidVotes;
+				const candidate = candidates[i];
+				if (this.officialCandidates.includes(candidate)) {
+					results[candidate] = candidateResult;
+				} else {
+					if (candidates[i].length === 0) {
+						blankVotes += votesWithoutDistricts[i];
+					} else {
+						nullVotes += votesWithoutDistricts[i];
+					}
+				}
+			}
+		} else {
+			for (districtVotes in Object.values(this.votesWithDistricts)) {
+				this.nbVotes += districtVotes.reduce((x, y) => x + y);
+			}
+
+			for (let i = 0; i < officialCandidates.size(); i++) {
+				const index = candidates.indexOf(officialCandidates[i]);
+
+				for (districtVotes in Object.values(this.votesWithDistricts)) {
+					this.nbValidVotes += districtVotes[index];
+				}
+			}
+
+			const officialCandidatesResult = {};
+
+			for (let i = 0; i < officialCandidates.length; i++) {
+				officialCandidatesResult[candidates[i]] = 0;
+			}
+			for (districtVotes in Object.values(this.votesWithDistricts)) {
+				const districtResult = [];
+				for (let i = 0; i < this.districtVotes.length; i++) {
+					let candidateResult = 0;
+					if (nbValidVotes != 0)
+						candidateResult =
+							(districtVotes[i] * 100) / nbValidVotes;
+					const candidate = candidates.get(i);
+					if (this.officialCandidates.includes(candidate)) {
+						districtResult.push(candidateResult);
+					} else {
+						if (candidates[i].length === 0) {
+							blankVotes += districtVotes[i];
+						} else {
+							nullVotes += districtVotes[i];
+						}
+					}
+				}
+				let districtWinnerIndex = 0;
+				for (let i = 1; i < districtResult.length; i++) {
+					if (
+						districtResult.get(districtWinnerIndex) <
+						districtResult[i]
+					)
+						districtWinnerIndex = i;
+				}
+				officialCandidatesResult[
+					this.candidates.get[districtWinnerIndex]
+				] += 1;
+			}
+			for (let i = 0; i < officialCandidatesResult.size(); i++) {
+				let ratioCandidate =
+					(this.officialCandidatesResult[candidates[i]] /
+						this.officialCandidatesResult.length) *
+					100;
+				results[this.candidates[i]] = ratioCandidate;
+			}
+		}
+
+		const blankResult = (blankVotes * 100) / nbVotes;
+		results.Blank = blankResult;
+
+		const nullResult = (nullVotes * 100) / nbVotes;
+		results.Null = nullResult;
+
+		const nbElectors = Object.values(this.list)
+			.map(x => x.length)
+			.reduce((x, y) => x + y);
+		const abstentionResult = 100 - (nbVotes * 100) / nbElectors;
+		results['Abstention'] = abstentionResult;
+
+		return results;
+	}
 }
